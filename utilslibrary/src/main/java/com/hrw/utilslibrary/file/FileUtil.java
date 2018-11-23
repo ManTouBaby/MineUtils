a453f5e1633aeca06933de677cbb65993ab15a0b
GitTest <https://github.com/ManTouBaby/Test.git>
2018-11-23 22:36:39 +0800

    工具初次提交

---
+++ utilslibrary/src/main/java/com/hrw/utilslibrary/file/FileUtil.java
@@ -0,0 +1,22 @@
+package com.hrw.utilslibrary.file;
+
+import android.content.Context;
+import android.os.Environment;
+
+/**
+ * @author:MtBaby
+ * @date:2018/11/23 22:31
+ * @desc:
+ */
+public class FileUtil {
+
+    public static String getAppPath(Context context) {
+        return getExternalStorageDirectory() + "/" + context.getPackageName();
+    }
+
+    public static String getExternalStorageDirectory() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath();
+    }
+
+
+}
